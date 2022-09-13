6fe6f7a874d61eab723a90dc1114631955fecb79
Dušan <dusan.uveric@mitigate.dev>
2022-09-13 22:15:51 +0200

    mongo fix
    Signed-off-by: Dušan <dusan.uveric@mitigate.dev>

--- app.rb
+++ app.rb
@@ -8,9 +8,7 @@ Dir[File.expand_path('config/initializers', __dir__) + '/**/*.rb'].sort.each do 
   require file
 end
 
-if ENV['MONGOID_ENABLED']
-  Mongoid.load!(File.expand_path('config/mongoid.yml', __dir__), ENV['RACK_ENV'])
-  require_relative 'lib/models'
-end
+Mongoid.load!(File.expand_path('config/mongoid.yml', __dir__), ENV['RACK_ENV'])
+require_relative 'lib/models'
 
 require_relative 'lib/slash_commands'
